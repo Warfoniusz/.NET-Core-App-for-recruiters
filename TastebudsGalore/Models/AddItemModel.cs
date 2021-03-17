ce73c32b248076389a1984c7f689451726af29c8
Wurf <havky1@gmail.com>
2021-03-17 19:54:29 +0100

    Added POST handling and a form to add a new product.

---
+++ TastebudsGalore/Models/AddItemModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TastebudsGalore.Models
+{
+    public class AddItemModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public IFormFile Picture { get; set; }
+    }
+}
--- TastebudsGalore/Pages/Admin/Add.cshtml.cs
+++ TastebudsGalore/Pages/Admin/Add.cshtml.cs
@@ -1,16 +1,67 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TastebudsGalore.Data;
+using TastebudsGalore.Models;
 
 namespace TastebudsGalore.Pages.Admin
 {
-    public class AddModel : PageModel
+    public class AddPageModel : PageModel
     {
-        public void OnGet()
+        private PizzaContext _Context;
+        public AddPageModel(PizzaContext context)
         {
+            _Context = context;
+        }
+
+        [BindProperty]
+        public AddItemModel AIM { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var item = new Item
+            {
+                Price = AIM.Price,
+            };
+
+            var product = new Product
+            {
+                Name = AIM.Name,
+                Description = AIM.Description,
+                Item = item
+            };
+
+            _Context.Products.Add(product);
+            _Context.SaveChanges();
+            product.ItemId = product.Id;
+            _Context.SaveChanges();
+
+            //This is very rough as it assumes you upload a JPG, should detect what kind of picture you upload
+            var filePath = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                "wwwroot",
+                "images",
+                product.Id + ".jpg"
+                );
+
+            if(AIM.Picture?.Length > 0)
+            {
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    AIM.Picture.CopyTo(stream);
+                }
+            }
+
+            return RedirectToPage("Index");
         }
     }
 }
