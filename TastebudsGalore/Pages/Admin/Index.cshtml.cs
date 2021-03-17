b652f75764fa564a1a43c36c81a52b0c65158869
Wurf <havky1@gmail.com>
2021-03-17 17:25:05 +0100

    Modified AdminHome, added GET handling for a product list.

--- TastebudsGalore/Pages/Admin/Index.cshtml.cs
+++ TastebudsGalore/Pages/Admin/Index.cshtml.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TastebudsGalore.Data;
+using TastebudsGalore.Models;
 
 namespace TastebudsGalore.Pages.Admin
 {
     public class IndexModel : PageModel
     {
+        private readonly PizzaContext _Context;
+
+        public IndexModel(PizzaContext context)
+        {
+            _Context = context;
+        }
+        public IEnumerable<Product> PizzaTypes { get; set; }
         public void OnGet()
         {
+            PizzaTypes = _Context.Products.ToList();
         }
     }
 }
+
